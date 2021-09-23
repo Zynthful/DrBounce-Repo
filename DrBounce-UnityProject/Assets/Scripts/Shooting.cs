160568cf4977bd84c410697f6ed46fa2ac3ee4f3
Luke Ryan <lr228874@falmouth.ac.uk>
2021-09-23 14:51:18 +0100

    Added fire rate and made amount of charges given a new variable (also comments)

--- DrBounce-UnityProject/Assets/Scripts/Shooting.cs
+++ DrBounce-UnityProject/Assets/Scripts/Shooting.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    private int chargesLeft = 0; //gets set to 3
-    [SerializeField] private int damage = 1;
-    [SerializeField] private int damageModifier = 2;
-    private int baseDamage = 1;
-    private int amountOfBounces = 0;
+    [Header("Damage")]
+    [SerializeField] private int baseDamage = 1;    //default damage of the gun 
+    [SerializeField] private int damageModifier = 2;    //multiplied with the amount of bounces 
+    private int damage = 1;     //current damage value
+
+    [Header("Charges")]
+    private int amountOfBounces = 0;    //amount of times the gun has been bounced successfully
+    [SerializeField] private int amountOfChargesGiven = 3;   //amount of charges given when you rebound the gun
+    private int chargesLeft = 0;    //current amount of charges left in the gun (reset if dropped)
+
+    [Header("Fire Rate")]
+    [SerializeField] private float fireRate = 0.9f;    //time between shots
+    private float timeSinceLastShot = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +29,8 @@ public class Shooting : MonoBehaviour
     void Update()
     {
         Shoot();
+
+        timeSinceLastShot += Time.deltaTime;
     }
 
     private void OnEnable()
@@ -36,8 +47,10 @@ public class Shooting : MonoBehaviour
 
     private void Shoot() 
     {
-        if (transform.parent != null && Input.GetMouseButtonDown(0))
+        if (transform.parent != null && Input.GetMouseButtonDown(0) && timeSinceLastShot > fireRate)    //checks the object has a parent and is not already shooting
         {
+            timeSinceLastShot = 0;
+
             if (chargesLeft > 0)
             {
                 damage = amountOfBounces * damageModifier;
@@ -49,14 +62,14 @@ public class Shooting : MonoBehaviour
             }
 
             Debug.LogWarning("BANG!!!");
-            Debug.LogWarning("You hit for " + damage);
+            Debug.LogWarning("You shot for " + damage);
         }
     }
 
     private void Bounce() 
     {
         amountOfBounces++;
-        chargesLeft = 3;
+        chargesLeft = amountOfChargesGiven;
     }
 
     private void Reset()
