50d80f8889fddebc8b60ef884f9764b5ee2671cc
JAMIE CARNELL <jc228467@falmouth.ac.uk>
2021-10-01 17:17:16 +0100

    Removed Health slider dependency in Health.cs and reworked it to use Event System
     - Created OnPlayerHeal, OnPlayerHealthChange and OnPlayerTakeDamage events
     - Made Canvas_PlayerHUD into a prefab

--- DrBounce-UnityProject/Assets/Scripts/Health.cs
+++ DrBounce-UnityProject/Assets/Scripts/Health.cs
@@ -5,46 +5,51 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    private int health = 1;
-
+    private int health = 100;
     private int maxHealth = 100;
 
     public delegate void CurrentHealth();
     public static event CurrentHealth ReportHealth;
 
-    [SerializeField] private Slider slider = null;
+    [Header("Events")]
+    // Passes health percentage
+    [SerializeField]
+    private GameEventFloat onHealthChange = null;
+    // Passes damage taken
+    [SerializeField]
+    private GameEventFloat onDamage = null;
+    // Passes health healed
+    [SerializeField]
+    private GameEventFloat onHeal = null;
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
-        UpdateUI();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        onHealthChange?.Raise(100.0f * ((float)health / (float)maxHealth));
     }
 
     private void Heal(int amount) 
     {
-        health += amount;
+        onHeal?.Raise(amount);
 
-        UpdateUI();
+        health += amount;
 
         if (health > maxHealth) 
         {
-            health = maxHealth;
-            
+            health = maxHealth;  
         }
+
+        onHealthChange?.Raise(100.0f * ((float)health / (float)maxHealth));
     }
 
     private void Damage(int amount) 
     {
+        onDamage?.Raise(amount);
+
         health -= amount;
 
-        UpdateUI();
+        onHealthChange?.Raise(100.0f * ((float) health / (float)maxHealth));
 
         if (health <= 0) 
         {
@@ -63,16 +68,6 @@ public class Health : MonoBehaviour
     private void DIE() 
     {
         Debug.Log("DIE (►__◄)");
-    }
-
-    private void UpdateUI()
-    {
-        if(slider != null)
-        {
-            float healthPercent = ((float)health / (float)maxHealth) * 100;
-            slider.value = healthPercent;
-        }
-
     }
 
     private void OnEnable()
