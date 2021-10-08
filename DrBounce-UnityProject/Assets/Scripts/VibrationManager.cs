d99e48d98d8292debb593de9ac9616bb8b2f7475
JACOB GIBBINS <Jg214708@falmouth.ac.uk>
2021-10-08 14:54:55 +0100

    Vibration Manager Script has been set up for multiple vibrations

--- DrBounce-UnityProject/Assets/Scripts/VibrationManager.cs
+++ DrBounce-UnityProject/Assets/Scripts/VibrationManager.cs
@@ -7,11 +7,34 @@ public class VibrationManager : MonoBehaviour
 {
 
     public HapticTypes dashHapticType = HapticTypes.LightImpact;
+    public HapticTypes shootingHapticType = HapticTypes.LightImpact;
+    public HapticTypes ChargedShootingHapticType = HapticTypes.LightImpact;
+    public HapticTypes TakeDamageHapticType = HapticTypes.LightImpact;
+    public HapticTypes CatchingHapticType = HapticTypes.LightImpact;
     // Start is called before the first frame update
- 
+
     public void DashVibration()
     {
         MMVibrationManager.Haptic(dashHapticType, false, true, this);
     }
 
+    public void BasicShotVibration()
+    {
+        MMVibrationManager.Haptic(shootingHapticType, false, true, this);
+    }
+
+    public void ChargedShotVibration()
+    {
+        MMVibrationManager.Haptic(ChargedShootingHapticType, false, true, this);
+    }
+
+    public void TakeDamageVibration()
+    {
+        MMVibrationManager.Haptic(TakeDamageHapticType, false, true, this);
+    }
+
+    public void CatchVibration()
+    {
+        MMVibrationManager.Haptic(CatchingHapticType, false, true, this);
+    }
 }
