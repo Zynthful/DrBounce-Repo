d2e0d8eebe3eb7e5ce4acf4767403a429b2208bf
Jacob Gibbins <JG214708@falmouth.ac.uk>
2021-10-09 16:25:29 +0100

    Organised the Vibration Manager script a bit more

--- DrBounce-UnityProject/Assets/Scripts/VibrationManager.cs
+++ DrBounce-UnityProject/Assets/Scripts/VibrationManager.cs
@@ -5,13 +5,19 @@ using MoreMountains.NiceVibrations;
 
 public class VibrationManager : MonoBehaviour
 {
-
+    //List of vibration Haptic types
+    
+    [Header("Movement")]
     public HapticTypes dashHapticType = HapticTypes.None;
     public HapticTypes jumpHapticType = HapticTypes.None;
+    [Header("Shooting")]
     public HapticTypes shootingHapticType = HapticTypes.None;
     public HapticTypes ChargedShootingHapticType = HapticTypes.None;
+    [Header("Misc")]
     public HapticTypes TakeDamageHapticType = HapticTypes.None;
     public HapticTypes CatchingHapticType = HapticTypes.None;
+    public HapticTypes activeChargeHapticType = HapticTypes.None;
+    public HapticTypes magnetAimAssistHapticType = HapticTypes.None;
     // Start is called before the first frame update
 
     public void DashVibration()
@@ -42,5 +48,15 @@ public class VibrationManager : MonoBehaviour
     public void JumpVibration()
     {
         MMVibrationManager.Haptic(jumpHapticType, false, true, this);
+    }
+
+    public void MagnetAimAssitVibration()
+    {
+        MMVibrationManager.Haptic(magnetAimAssistHapticType, false, true, this);
+    }
+
+    public void ActiveChargeVibration()
+    {
+        MMVibrationManager.Haptic(activeChargeHapticType, false, true, this);
     }
 }
