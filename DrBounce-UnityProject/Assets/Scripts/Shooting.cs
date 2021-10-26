31b27b91493eed3c03745c29edfa24338a710652
Luke Ryan <lr228874@falmouth.ac.uk>
2021-10-26 21:35:18 +0100

    Damange of charge bullets now work with damage graph
    Higher values are not working at the moment

--- DrBounce-UnityProject/Assets/Scripts/Shooting.cs
+++ DrBounce-UnityProject/Assets/Scripts/Shooting.cs
@@ -192,6 +192,9 @@ public class Shooting : MonoBehaviour
             case GunModes.Explosives:
                 FirstChargedShotFeedback?.PlayFeedbacks();
                 vibrationManager.ChargedShotVibration();
+
+                shooter.chargeBullet.damage = DamageAmountCalc(amountOfBounces);
+
                 GameObject obj = pool.SpawnBulletFromPool("ExplosiveShot", (PlayerMovement.player.position + (Vector3.up * (PlayerMovement.player.localScale.y / 8f))) + (fpsCam.transform.TransformDirection(Vector3.forward).normalized * 2.5f), Quaternion.Euler(fpsCam.transform.TransformDirection(Vector3.forward)), fpsCam.transform.TransformDirection(Vector3.forward).normalized, shooter.chargeBullet, null);
                 obj.GetComponent<ExplosiveShot>().comboSize = amountOfBounces;
                 //Reset();
