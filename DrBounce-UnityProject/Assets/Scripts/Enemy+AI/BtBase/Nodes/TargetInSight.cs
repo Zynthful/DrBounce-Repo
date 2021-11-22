91b33b5ea68e02c16c6391595f0802ba78faa890
Luke Ryan <lr228874@falmouth.ac.uk>
2021-11-22 14:45:10 +0000

    updated all new actions to act as nodes

--- DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/TargetInSight.cs
+++ DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/TargetInSight.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetInSight : MonoBehaviour
+public class TargetInSight : BtNode
 {
-    // Start is called before the first frame update
-    void Start()
+    public delegate NodeState ActionNodeDelegate(Blackboard blackboard);
+    private ActionNodeDelegate m_action;
+
+    public TargetInSight(ActionNodeDelegate action)
     {
-        
+        m_action = action;
+        reset();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override NodeState evaluate(Blackboard blackboard)
     {
-        
+        if (m_nodeState != NodeState.RUNNING)
+        {
+            return m_nodeState;
+        }
+
+        switch (m_action(blackboard))
+        {
+            case NodeState.SUCCESS:
+                m_nodeState = NodeState.SUCCESS;
+                return m_nodeState;
+            case NodeState.RUNNING:
+                m_nodeState = NodeState.RUNNING;
+                return m_nodeState;
+            case NodeState.FAILURE:
+            default:
+                m_nodeState = NodeState.FAILURE;
+                return m_nodeState;
+        }
+    }
+
+    public override string getName()
+    {
+        return "Action";
     }
 }
