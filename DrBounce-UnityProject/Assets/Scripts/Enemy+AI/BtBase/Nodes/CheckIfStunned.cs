77fc06f69720dc7688837126ec40fde35c78e9f8
Luke Ryan <lr228874@falmouth.ac.uk>
2021-11-22 14:56:07 +0000

    forgot to added check if stunned action

---
+++ DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/CheckIfStunned.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfStunned : BtNode
+{
+    public delegate NodeState ActionNodeDelegate(Blackboard blackboard);
+    private ActionNodeDelegate m_action;
+
+    public CheckIfStunned(ActionNodeDelegate action)
+    {
+        m_action = action;
+        reset();
+    }
+
+    public override NodeState evaluate(Blackboard blackboard)
+    {
+        if (m_nodeState != NodeState.RUNNING)
+        {
+            return m_nodeState;
+        }
+
+        switch (m_action(blackboard))
+        {
+            case NodeState.SUCCESS:
+                m_nodeState = NodeState.SUCCESS;
+                return m_nodeState;
+            case NodeState.RUNNING:
+                m_nodeState = NodeState.RUNNING;
+                return m_nodeState;
+            case NodeState.FAILURE:
+            default:
+                m_nodeState = NodeState.FAILURE;
+                return m_nodeState;
+        }
+    }
+
+    public override string getName()
+    {
+        return "Action";
+    }
+}
