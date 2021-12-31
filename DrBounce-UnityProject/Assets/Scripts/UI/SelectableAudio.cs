c5a22ece5fe959d91066ad834a2cb4ce2fb5b8d1
Jamie Carnell <jc228467@falmouth.ac.uk>
2021-12-31 16:25:06 +0000

    Setup functionality for selectable and button audio:
     - Created SelectableAudio class which posts OnSelect and OnPointerEnter AkEvents
     - Created ButtonAudio class (derived from SelectableAudio) which posts OnClickSuccess and OnClickFail AkEvents. OnClickSuccess plays if the button was interactable when clicked, and vice versa for OnClickFail.

---
+++ DrBounce-UnityProject/Assets/Scripts/UI/SelectableAudio.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Selectable))]
+public class SelectableAudio : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+    protected Selectable selectable;
+
+    [Header("Selectable AkEvents")]
+    [SerializeField]
+    protected AK.Wwise.Event onSelect = null;
+    [SerializeField]
+    protected AK.Wwise.Event onPointerEnter = null;
+
+    protected virtual void Awake()
+    {
+        if (selectable == null)
+        {
+            selectable = GetComponent<Selectable>();
+        }
+    }
+
+    // Called when the cursor enters the rect area of this selectable UI object.
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        if (selectable != null && selectable.IsInteractable())
+        {
+            onPointerEnter?.Post(gameObject);
+        }
+    }
+
+    // Called when the selectable UI object is selected.
+    public virtual void OnSelect(BaseEventData eventData)
+    {
+        onSelect?.Post(gameObject);
+    }
+}
