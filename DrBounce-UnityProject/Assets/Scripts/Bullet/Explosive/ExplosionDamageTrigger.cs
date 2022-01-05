4112c01a93aace8ea24f30ca32c93c2b64a74184
Isaac <iw230188@falmouth.ac.uk>
2022-01-05 16:17:04 +0000

    Very quick fix for 0 damage bug
    The effects have been handled rather than the cause - shots now do the damage they're supposed to, however still get applied again to the previous shot.

--- DrBounce-UnityProject/Assets/Scripts/Bullet/Explosive/ExplosionDamageTrigger.cs
+++ DrBounce-UnityProject/Assets/Scripts/Bullet/Explosive/ExplosionDamageTrigger.cs
@@ -7,6 +7,7 @@ public class ExplosionDamageTrigger : MonoBehaviour
     public int damage;
     private void OnTriggerEnter(Collider other) 
     {
+        damage = GetComponentInParent<ExplosiveShot>().dam;
         EnemyHealth check = other.GetComponent<EnemyHealth>();
         if(check)
         {
