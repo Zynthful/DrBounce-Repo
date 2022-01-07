2fb13d5ec6a254b1adb1e4eda3f143e722adbaf2
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-01-07 17:55:11 +0000

    Fixed death feedbacks causing timescale to permanently slow down:
     - Timescale modifier now lasts for 1.99 seconds as opposed to 2, whilst Fade lasts for 2, so the timescale resets just before the feedback is completed
     - thanks feel

--- DrBounce-UnityProject/Assets/Scripts/Scene Management/LoadScene.cs
+++ DrBounce-UnityProject/Assets/Scripts/Scene Management/LoadScene.cs
@@ -7,13 +7,11 @@ public class LoadScene : MonoBehaviour
 {
     public void LoadSceneByIndex(int index)
     {
-        Time.timeScale = 1;
         SceneManager.LoadScene(index);
     }
 
     public void LoadSceneByName(string name)
     {
-        Time.timeScale = 1;
         SceneManager.LoadScene(name);
     }
 
