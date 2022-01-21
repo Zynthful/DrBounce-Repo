1b4ccde78948739cb568e2a1253e99ca83ae2404
Isaac <iw230188@falmouth.ac.uk>
2022-01-21 15:10:03 +0000

    NoBounce AI full functionality
    A bit rough around the edges - but it's all there! Need to tidy up and optimise the script, as well as implement a more secure way of letting them know they're back at their waypoints.

--- DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/TargetInSight.cs
+++ DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/TargetInSight.cs
@@ -11,6 +11,7 @@ public class TargetInSight : BtNode
     private float m_viewDist;
     private float m_sightAngle;
     public bool m_canSeePlayer;
+    private bool spottedPlayer;
     private NavMeshAgent m_navMeshAgent;
 
     /// <summary>
@@ -40,8 +41,12 @@ public class TargetInSight : BtNode
         // If player is in Line of Sight, set run blackboard.target.NewTarget(true, (player gameobject here)); to assign new target values to enemy AI
         enemyPosition = blackboard.owner.transform;
 
-        Debug.Log("Successfuly reached " + getName());
-        Debug.Log(m_blackboard.searchTime);
+        //Debug.Log("Successfuly reached " + getName());
+
+        if(spottedPlayer == true)
+        {
+            m_navMeshAgent.destination = PlayerMovement.player.transform.position;
+        }
 
         if (PlayerLosCheck())
         {
@@ -50,10 +55,11 @@ public class TargetInSight : BtNode
 
             if (m_blackboard.noBounceAIController != null)
             {
+                m_blackboard.searchTime = 0;
+                spottedPlayer = true;
                 //Enables the navmesh if the player is spotted, disables use of waypoints and sets the player as the navmesh target.
                 m_blackboard.noBounceAIController.canMove = false;
                 m_navMeshAgent.enabled = true;
-                m_navMeshAgent.destination = m_blackboard.target.playerObject.transform.position;
             }
 
             return NodeState.SUCCESS;
@@ -88,10 +94,14 @@ public class TargetInSight : BtNode
                 if (m_blackboard.noBounceAIController != null)
                 {
                     //Currently only uses time spent searching to de-activate, should probably also de-activate if the player is too far.
-                    countDown();
                 }
                 Debug.DrawLine(ray.origin, ray.origin + (PlayerMovement.player.position - enemyPosition.position).normalized * m_viewDist, Color.red);
             }
+        }
+        else if(spottedPlayer == true)
+        {
+            Debug.Log(m_blackboard.searchTime);
+            countDown();
         }
         return false;
     }
@@ -100,12 +110,20 @@ public class TargetInSight : BtNode
     {
         //Counts down from 0 to -5
         m_blackboard.searchTime -= Time.deltaTime;
-        if(m_blackboard.searchTime <= -5)
+        if(m_blackboard.searchTime <= -10)
         {
+            spottedPlayer = false;
             //resets timer
             m_blackboard.searchTime = 0;
             //disables navmesh if the enemy can't find the player and returns to its waypoints.
-            m_navMeshAgent.enabled = false;
+
+            m_navMeshAgent.destination = m_blackboard.noBounceAIController.patrolPoints[0];
+
+            //If enemy's x value is close to the waypoint location
+            if(m_blackboard.noBounceAIController.patrolPoints[0].x >= m_blackboard.noBounceAIController.transform.position.x - 5 && m_blackboard.noBounceAIController.patrolPoints[0].x <= m_blackboard.noBounceAIController.transform.position.x + 5)
+            {
+                m_navMeshAgent.enabled = false;
+            }
             m_blackboard.noBounceAIController.canMove = true;
         }
     }
