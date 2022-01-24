7878c199e129b35f04994650efc2319579019bee
Isaac <iw230188@falmouth.ac.uk>
2022-01-24 14:54:03 +0000

    Fixed Bug where enemy would not reset sight timer

--- DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/TargetInSight.cs
+++ DrBounce-UnityProject/Assets/Scripts/Enemy+AI/BtBase/Nodes/TargetInSight.cs
@@ -42,10 +42,29 @@ public class TargetInSight : BtNode
         enemyPosition = blackboard.owner.transform;
 
         //Debug.Log("Successfuly reached " + getName());
+        Debug.Log(m_blackboard.searchTime);
 
-        if(spottedPlayer == true)
+        if (spottedPlayer == true && m_blackboard.noBounceAIController != null)
         {
+            m_navMeshAgent.enabled = true;
             m_navMeshAgent.destination = PlayerMovement.player.transform.position;
+
+            if (m_blackboard.searchTime <= -10)
+            {
+                spottedPlayer = false;
+                //resets timer
+                m_blackboard.searchTime = 0;
+                //disables navmesh if the enemy can't find the player and returns to its waypoints.
+
+                m_navMeshAgent.destination = m_blackboard.noBounceAIController.patrolPoints[0];
+
+                //If enemy's x value is close to the waypoint location
+                if (m_blackboard.noBounceAIController.patrolPoints[0].x >= m_blackboard.noBounceAIController.transform.position.x - 5 && m_blackboard.noBounceAIController.patrolPoints[0].x <= m_blackboard.noBounceAIController.transform.position.x + 5)
+                {
+                    m_navMeshAgent.enabled = false;
+                }
+                m_blackboard.noBounceAIController.canMove = true;
+            }
         }
 
         if (PlayerLosCheck())
@@ -55,11 +74,8 @@ public class TargetInSight : BtNode
 
             if (m_blackboard.noBounceAIController != null)
             {
-                m_blackboard.searchTime = 0;
-                spottedPlayer = true;
                 //Enables the navmesh if the player is spotted, disables use of waypoints and sets the player as the navmesh target.
                 m_blackboard.noBounceAIController.canMove = false;
-                m_navMeshAgent.enabled = true;
             }
 
             return NodeState.SUCCESS;
@@ -87,21 +103,17 @@ public class TargetInSight : BtNode
             if (Physics.Raycast(ray, out hit, m_viewDist) && hit.transform.root.CompareTag("Player"))
             {
                 Debug.DrawLine(ray.origin, ray.origin + (PlayerMovement.player.position - enemyPosition.position).normalized * m_viewDist, Color.green);
+                spottedPlayer = true;
                 return true;
+            }
+            else if (spottedPlayer == true && m_blackboard.noBounceAIController != null)
+            {
+                countDown();
             }
             else
             {
-                if (m_blackboard.noBounceAIController != null)
-                {
-                    //Currently only uses time spent searching to de-activate, should probably also de-activate if the player is too far.
-                }
                 Debug.DrawLine(ray.origin, ray.origin + (PlayerMovement.player.position - enemyPosition.position).normalized * m_viewDist, Color.red);
             }
-        }
-        else if(spottedPlayer == true)
-        {
-            Debug.Log(m_blackboard.searchTime);
-            countDown();
         }
         return false;
     }
@@ -110,22 +122,6 @@ public class TargetInSight : BtNode
     {
         //Counts down from 0 to -5
         m_blackboard.searchTime -= Time.deltaTime;
-        if(m_blackboard.searchTime <= -10)
-        {
-            spottedPlayer = false;
-            //resets timer
-            m_blackboard.searchTime = 0;
-            //disables navmesh if the enemy can't find the player and returns to its waypoints.
-
-            m_navMeshAgent.destination = m_blackboard.noBounceAIController.patrolPoints[0];
-
-            //If enemy's x value is close to the waypoint location
-            if(m_blackboard.noBounceAIController.patrolPoints[0].x >= m_blackboard.noBounceAIController.transform.position.x - 5 && m_blackboard.noBounceAIController.patrolPoints[0].x <= m_blackboard.noBounceAIController.transform.position.x + 5)
-            {
-                m_navMeshAgent.enabled = false;
-            }
-            m_blackboard.noBounceAIController.canMove = true;
-        }
     }
     public override string getName()
     {
