1017fff3f2160b63711aa0bacbd165a1c0934ece
TC231931 <tc231931@falmouth.ac.uk>
2022-01-24 17:32:05 +0000

    the slimes were garfield all along

--- DrBounce-UnityProject/Assets/Scripts/Particles/ParticleSlime.cs
+++ DrBounce-UnityProject/Assets/Scripts/Particles/ParticleSlime.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ParticleSlime : MonoBehaviour
 {
     private DecalManager decalM;
-    [SerializeField] Material decal = null;
 
     private void Start()
     {
@@ -14,18 +13,18 @@ public class ParticleSlime : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log(other.name);
-
         List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
 
         ParticlePhysicsExtensions.GetCollisionEvents(this.GetComponent<ParticleSystem>(), other, collisionEvents);
 
         for (int i = 0; i < collisionEvents.Count; i++)
         {
+            //Debug.Log((collisionEvents[i].colliderComponent.gameObject.name) + ", new particle spawn :)");
+
             Vector3 pos = collisionEvents[i].intersection;
             Vector3 normal = collisionEvents[i].normal;
 
-            decalM.SpawnDecal(pos, normal, 1f, DecalManager.DecalType.placeholder);
+            decalM.SpawnDecal(pos, normal, 1f, DecalManager.DecalType.slime);
         }
     }
 }
