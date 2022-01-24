07f4be47fa005655ed044a072c08e56b247f1008
TC231931 <tc231931@falmouth.ac.uk>
2022-01-24 16:32:42 +0000

    Particles can now spawn slime (garfield) with this cool new script

---
+++ DrBounce-UnityProject/Assets/Scripts/Particles/ParticleSlime.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSlime : MonoBehaviour
+{
+    private DecalManager decalM;
+    [SerializeField] Material decal = null;
+
+    private void Start()
+    {
+        decalM = DecalManager.Instance;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log(other.name);
+
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+
+        ParticlePhysicsExtensions.GetCollisionEvents(this.GetComponent<ParticleSystem>(), other, collisionEvents);
+
+        for (int i = 0; i < collisionEvents.Count; i++)
+        {
+            Vector3 pos = collisionEvents[i].intersection;
+            Vector3 normal = collisionEvents[i].normal;
+
+            decalM.SpawnDecal(pos, normal, 1f, decal);
+        }
+    }
+}
