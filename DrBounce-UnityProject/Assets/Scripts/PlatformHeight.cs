cdcd3ae7275db4cd396cfce9a73673126cfbd5cd
TC231931 <tc231931@falmouth.ac.uk>
2022-02-09 19:56:02 +0000

    Updated floor, fixed platform height script, increased per pixel light limit

--- DrBounce-UnityProject/Assets/Scripts/PlatformHeight.cs
+++ DrBounce-UnityProject/Assets/Scripts/PlatformHeight.cs
@@ -11,16 +11,35 @@ public class PlatformHeight : MonoBehaviour
     [SerializeField]
     private float height = 1f;
 
+    [SerializeField]
+    private float baseSizeY = 0.1f;
+
+    [SerializeField]
+    private float topSizeY = 0.1f;
+
+    [SerializeField]
+    private float pieceSizeY = 0.5f;
+
+    private void Start()
+    {
+        if (Application.IsPlaying(gameObject))
+        {
+            Destroy(this);
+        }
+    }
+
     private void Update()
     {
-        float newHeight = height + 0.1f;
-        float pieceHeight = newHeight / 6f;
+        float baseAndTopHeight = topSizeY + baseSizeY;
+        float baseToTopHeight = height - baseAndTopHeight;
+        float pieceHeight = baseToTopHeight / 6f;
 
         for (int i = 0; i < 6; i++)
         {
-            objects[i].localPosition = pieceHeight * i * Vector3.up;
+            objects[i].localPosition = pieceHeight * i * Vector3.up + (baseSizeY * Vector3.up);
         }
 
-        objects[6].localPosition = (0.75f + newHeight - (newHeight / 6f)) * Vector3.up;
+        objects[6].localPosition = objects[5].localPosition + ((pieceSizeY + baseSizeY - topSizeY) * Vector3.up);
+        //objects[6].localPosition = objects[5].localPosition + ((pieceSizeY * Vector3.up) + (baseSizeY * Vector3.up) - (topSizeY * Vector3.up));
     }
 }
