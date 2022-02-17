11d94a3c74e8e2c826f5791d3d6ce8a6fc6c129d
COLE GILBERT <cg233192@falmouth.ac.uk>
2022-02-17 10:42:53 +0000

    Setup the base unlocking mechanic framework

---
+++ DrBounce-UnityProject/Assets/Scripts/Player/UnlockTracker.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnlockTracker : MonoBehaviour
+{
+    [System.Serializable]
+    public enum UnlockTypes
+    {
+        FirstDash,
+        SecondDash,
+        Magnet,
+    }
+
+    [SerializeField]
+    private Unlocks levelStartSettings;
+
+
+    // Events to enable/disable unlocks
+    [SerializeField]
+    public UnityEvent unlockFirstDash = null;
+    [SerializeField]
+    public UnityEvent unlockSecondDash = null;
+    [SerializeField]
+    public UnityEvent unlockMagnet = null;
+
+    void EnableUnlock(UnlockTypes type)
+    {
+        switch(type)
+        {
+            case UnlockTypes.FirstDash:
+                unlockFirstDash.Invoke();
+                break;
+
+            case UnlockTypes.SecondDash:
+                unlockSecondDash.Invoke();
+                break;
+
+            case UnlockTypes.Magnet:
+                unlockMagnet.Invoke();
+                break;
+
+            default:
+                Debug.LogError("Type not setup with an event, @Cole for being dumb");
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(levelStartSettings != null)
+        {
+            foreach(UnlockTypes unlock in levelStartSettings.unlocks)
+            {
+                EnableUnlock(unlock);
+            }
+        }
+        else
+            Debug.Log("Running without any unlocks, have you forgotten to setup the scriptableObject?");
+    }
+}
