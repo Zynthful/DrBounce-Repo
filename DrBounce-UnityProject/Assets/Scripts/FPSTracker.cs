c745acad297fe4b836af83377ab079b92d62ce3a
LUKE RYAN <lr228874@falmouth.ac.uk>
2022-03-11 15:43:02 +0000

    fps tracker now has COLOURS

--- DrBounce-UnityProject/Assets/Scripts/FPSTracker.cs
+++ DrBounce-UnityProject/Assets/Scripts/FPSTracker.cs
@@ -9,7 +9,13 @@ public class FPSTracker : MonoBehaviour
 
     public static FPSTracker s_Instance = null;
 
-    [SerializeField] private TMP_Text m_TextComponent;
+    [SerializeField] private TMP_Text colour;
+    [SerializeField] private TMP_Text fpsCounter;
+
+    [SerializeField] private Color badFPSColor = new Color();
+    [SerializeField] private Color okFPSColor = new Color();
+    [SerializeField] private Color goodFPSColor = new Color();
+    [SerializeField] private Color bestFPSColor = new Color();
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +50,35 @@ public class FPSTracker : MonoBehaviour
 
     void OutputTime()
     {
-        m_TextComponent.text = "FPS: " + (int)(1f / Time.unscaledDeltaTime);
+        fps = (int)(1f / Time.unscaledDeltaTime);
+
+        if (fps >= 120)
+        {
+            colour.color = bestFPSColor;
+            colour.text = "Golden";
+
+            fpsCounter.text = "FPS: " + fps;
+        }
+        if (fps >= 60)
+        {
+            colour.color = goodFPSColor;
+            colour.text = "Green";
+
+            fpsCounter.text = "FPS: " + fps;
+        }
+        else if (fps >= 30)
+        {
+            colour.color = okFPSColor;
+            colour.text = "Amber";
+
+            fpsCounter.text = "FPS: " + fps;
+        }
+        else 
+        {
+            colour.color = badFPSColor;
+            colour.text = "Red";
+
+            fpsCounter.text = "FPS: " + fps;
+        }
     }
 }
