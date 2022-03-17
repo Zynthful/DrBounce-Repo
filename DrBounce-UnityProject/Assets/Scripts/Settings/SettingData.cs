3abbb38523a109cd80b8919e6a2185bb27859c84
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-03-17 18:37:09 +0000

    Implemented toggle option for fps and average fps counters

--- DrBounce-UnityProject/Assets/Scripts/Settings/SettingData.cs
+++ DrBounce-UnityProject/Assets/Scripts/Settings/SettingData.cs
@@ -3,6 +3,21 @@ using UnityEngine;
 
 public abstract class SettingData : ScriptableObject
 {
+    [Header("Base Setting Info")]
+    [SerializeField]
+    protected string settingName = null;
+
+    [SerializeField]
+    protected SettingType type = new SettingType();
+    public SettingType GetSettingType() { return type; }
+
+    [SerializeField]
+    protected SettingSubType subType = new SettingSubType();
+    public SettingSubType GetSettingSubType() { return subType; }
+
+    public delegate void ResetDefault();
+    public event ResetDefault OnResetDefault;
+
     public enum SettingType
     {
         Game,
@@ -21,18 +36,6 @@ public abstract class SettingData : ScriptableObject
         Debug,
     }
 
-    [Header("Base Setting Info")]
-    [SerializeField]
-    protected string settingName = null;
-
-    [SerializeField]
-    protected SettingType type = new SettingType();
-    [SerializeField]
-    protected SettingSubType subType = new SettingSubType();
-
-    public delegate void ResetDefault();
-    public event ResetDefault OnResetDefault;
-
     protected void Save()
     {
         PlayerPrefs.Save();
@@ -49,15 +52,5 @@ public abstract class SettingData : ScriptableObject
         {
             OnResetDefault();
         }
-    }
-
-    public SettingType GetSettingType()
-    {
-        return type;
-    }
-
-    public SettingSubType GetSettingSubType()
-    {
-        return subType;
     }
 }
