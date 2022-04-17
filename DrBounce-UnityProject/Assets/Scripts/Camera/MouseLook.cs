81830bcdbf12c6d5d49fb5fcc2d3591e0d24400b
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-04-17 15:21:08 +0100

    Aim Sensitivity changes and optimisations
     - Aim sensitivity is no longer influenced by Time.deltaTime (which should fix inconsistent aim sensitivities at varying framerates)
     - Aim sensitivity now reads the relative sensitivity scriptable object directly instead of storing it as a local variable
     - Tweaked sensitivity ranges to 1-30 whole numbers (default: 10)

--- DrBounce-UnityProject/Assets/Scripts/Camera/MouseLook.cs
+++ DrBounce-UnityProject/Assets/Scripts/Camera/MouseLook.cs
@@ -8,50 +8,22 @@ public class MouseLook : MonoBehaviour
     [Header("Declarations")]
     [SerializeField]
     private Transform playerBody;
+
+    [Header("Sensitivity Settings")]
     [SerializeField]
-    private FloatSetting mouseSensSetting = null;
+    private FloatSetting mouseSensitivity = null;
     [SerializeField]
-    private FloatSetting controllerSensSetting = null;
-
-    [Header("Mouse Sensitivity Settings")]
-    //[SerializeField]
-    private float mouseSensitivityX = 0.0f;
-    //[SerializeField]
-    private float mouseSensitivityY = 0.0f;
-
-    private float currentMouseSensitivity = 4.0f;
-
-    [Header("Controller Sensitivity Settings")]
-    //[SerializeField]
-    private float controllerSensitivityX = 0.0f;
-    //[SerializeField]
-    private float controllerSensitivityY = 0.0f;
+    private FloatSetting controllerSensitivity = null;
     [SerializeField]
     private float controllerAssistSensitivityMultiplier = 0.5f;
 
-    private float currentControllerSensitivity = 7.0f;
-
     private float xRotation = 0f;
-
-    private float currentControllerSensitivityMultiplier = 1f;
 
     public Vector2 aimAssistInfluence { private get; set; } = Vector2.zero;
 
-    public InputMaster controls;
-
     private void Start()
     {
-        controls = InputManager.inputMaster;
-
         Cursor.lockState = CursorLockMode.Locked;
-
-        SetMouseSensitivity(mouseSensSetting.GetCurrentValue());
-        SetControllerSensitivity(controllerSensSetting.GetCurrentValue());
-
-        // mouseSensitivityX = mouseSensitivityY = currentMouseSensitivity;
-        // controllerSensitivityX = mouseSensitivityY = mouseSensitivity;
-        // controllerSensitivityX = currentMouseSensitivity;
-        // controllerSensitivityY = currentMouseSensitivity;
     }
 
     private void LateUpdate()
@@ -63,21 +35,21 @@ public class MouseLook : MonoBehaviour
 
         if (Gamepad.current != null)
         {
-            conX = Gamepad.current.rightStick.x.ReadValue() * controllerSensitivityX * 50.0f * Time.deltaTime;
-            conY = Gamepad.current.rightStick.y.ReadValue() * controllerSensitivityY * 50.0f * Time.deltaTime;
+            conX = Gamepad.current.rightStick.x.ReadValue() * controllerSensitivity.GetCurrentValue() * 0.4f;
+            conY = Gamepad.current.rightStick.y.ReadValue() * controllerSensitivity.GetCurrentValue() * 0.4f;
 
             // Old Aim assist
             //conX *= currentControllerSensitivityMultiplier;
             //conY *= currentControllerSensitivityMultiplier;
         }
 
-        float mouseX = (Mouse.current.delta.x.ReadValue() * mouseSensitivityX * 3.5f) * Time.deltaTime;
-        float mouseY = (Mouse.current.delta.y.ReadValue() * mouseSensitivityY * 3.5f) * Time.deltaTime;
+        float mouseX = Mouse.current.delta.x.ReadValue() * mouseSensitivity.GetCurrentValue() * 0.01f;
+        float mouseY = Mouse.current.delta.y.ReadValue() * mouseSensitivity.GetCurrentValue() * 0.01f;
 
         float camX = conX + mouseX;
         float camY = conY + mouseY;
 
-        Vector2 moveInput = controls.Player.Movement.ReadValue<Vector2>();
+        Vector2 moveInput = InputManager.inputMaster.Player.Movement.ReadValue<Vector2>();
 
         if((camX == 0 && camY == 0) && (moveInput.x == 0 && moveInput.y == 0))
         {
@@ -92,7 +64,7 @@ public class MouseLook : MonoBehaviour
             aimAssistInfluence = aimAssistInfluence / 1.5f;
         }
 
-        aimAssistInfluence /= controllerSensitivityX;
+        aimAssistInfluence /= controllerSensitivity.GetCurrentValue();
 
         camX -= aimAssistInfluence.x; camY -= aimAssistInfluence.y;
 
@@ -101,19 +73,6 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * camX);
-    }
-
-    public void SetMouseSensitivity(float value)
-    {
-        currentMouseSensitivity = value;
-        mouseSensitivityX = mouseSensitivityY = currentMouseSensitivity;
-    }
-
-    public void SetControllerSensitivity(float value)
-    {
-        currentControllerSensitivity = value;
-        controllerSensitivityX = currentControllerSensitivity;
-        controllerSensitivityY = currentControllerSensitivity;
     }
 
     public void IsHovering(bool hover)
