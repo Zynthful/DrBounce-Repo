4ea72f876661f474f2227203c918bc7882328995
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-04-22 19:03:21 +0100

    Extended door functionality, implemented door sfx and replaced some of the doors in lv2 with instances of the door prefab
     - Doors can now be manually Opened and Closed by calling the relevant methods on them
     - Doors now have an Initial State variable so a door can be set to be open by default
     - On enable, if the initial state is open, the door's given transforms will be updated with the open pos, rot and scale
     - Doors now have separate Open and Close feedbacks which are called by the door's OnOpen and OnClose events
     - Also put trigger zones onto the Ignore Raycast layer

--- DrBounce-UnityProject/Assets/Scripts/Rooms/Door.cs
+++ DrBounce-UnityProject/Assets/Scripts/Rooms/Door.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Door : MonoBehaviour
 {
-    [Header("Declarations")]
+    private bool open = false;
+
+    [Header("Door Settings")]
+
     [SerializeField]
     [Tooltip("All enemies below have to be dead for this door to open.")]
     private EnemyHealth[] enemies = null;
 
+    [SerializeField]
+    [Tooltip("Whether the door should start [OPEN] or [CLOSED].")]
+    private InitialState initialState = InitialState.Closed;
+
+    [SerializeField]
+    [Tooltip("If automatic, it will close when it detects an enemy is alive. If manual, it will only close when [Close()] is called.")]
+    private CloseSettings closeSettings = CloseSettings.Automatic;
+
+    [SerializeField]
+    [Tooltip("The transform information (position, rotation, scale) that should be applied to the given transforms when [Initial State] is set to [OPEN].")]
+    private OpenTransformInfo[] openTransformInfo;
+
     [Header("Unity Events")]
     public UnityEvent onOpen = null;
     public UnityEvent onClose = null;
+
+    private enum InitialState
+    {
+        Open,
+        Closed,
+    }
+
+    private enum CloseSettings
+    {
+        Automatic,
+        Manual,
+    }
+
+    [System.Serializable]
+    private struct OpenTransformInfo
+    {
+        public Transform transformToUpdate;
+        public Vector3 openPosition;
+        public Quaternion openRotation;
+        public Vector3 openScale;
+    }
 
     private void OnEnable()
     {
@@ -25,7 +59,34 @@ public class Door : MonoBehaviour
             }
         }
 
-        CheckIfCanOpen();
+        switch (initialState)
+        {
+            case InitialState.Open:
+                open = true;
+                foreach (OpenTransformInfo info in openTransformInfo)
+                {
+                    info.transformToUpdate.localPosition = info.openPosition;
+                    info.transformToUpdate.localRotation = info.openRotation;
+                    info.transformToUpdate.localScale = info.openScale;
+                }
+                break;
+            case InitialState.Closed:
+                open = false;
+                break;
+            default:
+                break;
+        }
+
+        switch (closeSettings)
+        {
+            case CloseSettings.Automatic:
+                CheckIfCanOpen();
+                break;
+            case CloseSettings.Manual:
+                break;
+            default:
+                break;
+        }
     }
 
     private void OnDisable()
@@ -40,13 +101,32 @@ public class Door : MonoBehaviour
         }
     }
 
-    private void Open() 
+    public void SetOpen(bool value)
     {
+        if (open == value)
+            return;
+
+        if (value)
+            Open();
+        else
+            Close();
+    }
+
+    public void Open() 
+    {
+        if (open)
+            return;
+
+        open = true;
         onOpen?.Invoke();
     }
 
-    private void Close() 
+    public void Close() 
     {
+        if (!open)
+            return;
+
+        open = false;
         onClose?.Invoke();
     }
 
@@ -68,13 +148,6 @@ public class Door : MonoBehaviour
             }
         }
 
-        if (!isAnEnemyAlive) 
-        {
-            Open();
-        }
-        else
-        {
-            Close();
-        }
+        SetOpen(!isAnEnemyAlive);
     }
 }
