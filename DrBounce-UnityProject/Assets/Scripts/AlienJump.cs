7b75cbd51ce438ae16a61256796246cda13c926d
ColeGGilbert <penguinojoe@gmail.com>
2022-04-27 16:12:04 +0100

    Removed old code from an *old* project

--- DrBounce-UnityProject/Assets/Scripts/AlienJump.cs
+++ DrBounce-UnityProject/Assets/Scripts/AlienJump.cs
@@ -5,15 +5,7 @@ using UnityEngine;
 public class AlienJump : MonoBehaviour
 {
     [SerializeField] private Animator anim = null;  //🐌
-    private float jumpTime = 4.0f;
     private bool celebrating = false;
-
-    private void Start()
-    {
-        //anim.GetComponent<Animator>();  //bad line 🐌🐌
-        anim.SetFloat("offset", Random.Range(0.0f, 2.0f));
-        StartCoroutine(WaitTime());
-    }
 
     // Update is called once per frame
     void Update()
@@ -27,35 +19,11 @@ public class AlienJump : MonoBehaviour
 
     private void Celebrate() 
     {
-        celebrating = true;
-        anim.SetTrigger("isCelebrating");
-    }
-
-    private void OnEnable()
-    {
-        IngGameController.OnServedCombo += Celebrate;
-    }
-
-    private void OnDisable()
-    {
-        IngGameController.OnServedCombo -= Celebrate;
-    }
-
-    private void Jump() 
-    {
-        if (!celebrating)
+        if(!celebrating)
         {
-            anim.SetTrigger($"Cheer{Random.Range(1, 4)}");
-            //anim.SetTrigger("Cheer1");
+            celebrating = true;
+            anim.SetTrigger("isCelebrating");
         }
-    }
-
-    private IEnumerator WaitTime() 
-    {
-        jumpTime = Random.Range(4.0f, 5.0f);
-        float waitTime = Random.Range(0.5f, 4.0f);
-        yield return new WaitForSeconds(waitTime);
-        InvokeRepeating("Jump", 3, jumpTime);
     }
 
     public void CelebrationEnd() 
