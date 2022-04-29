c0fb905ab1b179fb9aa5be24f29a5c6261306461
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-04-29 11:03:55 +0100

    Added small comments to CheckpointHit script

--- DrBounce-UnityProject/Assets/Scripts/Checkpoint/CheckpointHit.cs
+++ DrBounce-UnityProject/Assets/Scripts/Checkpoint/CheckpointHit.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CheckpointHit : MonoBehaviour
 {
+    // Event for when we hit this checkpoint
     public delegate void Hit(CheckpointHit checkpoint);
     public static event Hit onHit;
 
-    [Tooltip("Unique ID for this checkpoint. Each checkpoint should be ordered ascending by ID (i.e., later checkpoints = use higher id).")]
+    [Tooltip("Unique ID for this checkpoint. Each checkpoint should be ordered ascending by ID (i.e., later checkpoints = use higher id). When we hit this checkpoint, this ID will be used to set our current checkpoint.")]
     public int id = -1;
 
     private void Awake()
