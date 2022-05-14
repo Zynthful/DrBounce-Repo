793541873ea33b623db808f41d08092041823e0d
NATHAN OLADIPO <NO230675@falmouth.ac.uk>
2022-05-14 23:52:16 +0100

    tweaked recall reminder script to include events for feedbacks

--- DrBounce-UnityProject/Assets/Scripts/UI/RecallReminder.cs
+++ DrBounce-UnityProject/Assets/Scripts/UI/RecallReminder.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RecallReminder : MonoBehaviour
 {
     [SerializeField] private GameObject Text;
+    public UnityEvent OnRecallPrompt = null;
+    public UnityEvent OnRecallDone = null;
 
     private void Start()
     {
@@ -27,10 +30,12 @@ public class RecallReminder : MonoBehaviour
     private void TurnOn()
     {
         Text.SetActive(true);
+        OnRecallPrompt?.Invoke();
     }
 
     private void TurnOff()
     {
         Text.SetActive(false);
+        OnRecallDone?.Invoke();
     }
 }
